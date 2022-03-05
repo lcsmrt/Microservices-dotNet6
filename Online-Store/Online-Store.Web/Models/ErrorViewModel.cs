45b472c0be1c5e91ddd4bc40b8203ae347842f18
Lucas Martins <61764319+lcsmrt@users.noreply.github.com>
2022-03-05 02:41:23 -0300

    installed dependencies

---
+++ Online-Store/Online-Store.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Online_Store.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
